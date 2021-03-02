f989b38513aefc049f3ebdd3312484e2565cf910
Dylan Berry <contact@dylanberry.dev>
2021-03-01 20:23:03 -0700

    Working as intended

--- connect.js
+++ connect.js
@@ -13,14 +13,17 @@ const connect = async (bot, channel) => {
 
     bot.client.on('message', (channel, tags, message, self) => {
       message = message.trim().toLowerCase();
-      if (bot.users.some(user => user === tags['display-name'])) {
-        console.log('User already answered.');
-      } else if (message.split(' ').length > 1) {
+      // if (bot.users.some(user => user === tags['display-name'])) {
+      //   console.log('User already answered.');
+      // }
+      if (message.split(' ').length > 1) {
         console.log('User submitted more than 1 word.');
       } else {
         bot.users.push(tags['display-name']); // Push user to array
         if (bot.responses.some(response => response.text === message)) {
-          response.count++; // Increment word count if it already exists
+          const index = bot.responses.findIndex(response => response.text === message); // Get index of matching response
+          bot.responses[index].count++; // Increment word count if it already exists
+          io.emit('response', { responses: bot.responses, users: bot.users });
         } else {
           bot.responses.push({ text: message, count: 1 }); // Push word and set count to 1 if response doesn't exist
           io.emit('response', { responses: bot.responses, users: bot.users });
